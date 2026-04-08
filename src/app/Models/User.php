e5b8257fe7076302314ce4ca857bd9d358760a70
kenji kobayashi <kenzicolab.future@gmail.com>
2026-04-08 22:06:20 +0900

    ログイン画面、登録画面確認

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -7,6 +7,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use App\Models\Profile;
+use App\Models\Item;
+use App\Models\Like;
+use App\Models\Comment;
+use App\Models\Order;
 
 class User extends Authenticatable
 {
@@ -41,4 +46,29 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function profile()
+    {
+        return $this->hasOne(Profile::class);
+    }
+
+    public function items()
+    {
+        return $this->hasMany(Item::class);
+    }
+
+    public function likes()
+    {
+        return $this->hasMany(Like::class);
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    public function orders()
+    {
+        return $this->hasMany(Order::class);
+    }
 }
--- src/resources/views/auth/login.blade.php
+++ src/resources/views/auth/login.blade.php
@@ -7,13 +7,13 @@
 </head>
 <body>
     <h1>ログイン</h1>
-    <form method="POST" action="{{ route(\'login\') }}">
+    <form method="POST" action="{{ route('login') }}">
         @csrf
 
         <div>
             <label for="email">メールアドレス</label>
-            <input id="email" type="email" name="email" value="{{ old(\'email\') }}" required autofocus>
-            @error(\'email\')
+            <input id="email" type="email" name="email" value="{{ old('email') }}" required autofocus>
+            @error('email')
                 <span>{{ $message }}</span>
             @enderror
         </div>
@@ -21,13 +21,13 @@
         <div>
             <label for="password">パスワード</label>
             <input id="password" type="password" name="password" required autocomplete="current-password">
-            @error(\'password\')
+            @error('password')
                 <span>{{ $message }}</span>
             @enderror
         </div>
 
         <div>
-            <input type="checkbox" name="remember" id="remember" {{ old(\'remember\') ? \'checked\' : \'\' }}>
+            <input type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
             <label for="remember">ログイン情報を記憶する</label>
         </div>
 
@@ -35,12 +35,12 @@
             <button type="submit">ログイン</button>
         </div>
 
-        @if (Route::has(\'password.request\'))
-            <a href="{{ route(\'password.request\') }}">
+        @if (Route::has('password.request'))
+            <a href="{{ route('password.request') }}">
                 パスワードを忘れた場合
             </a>
         @endif
     </form>
-    <p><a href="{{ route(\'register\') }}">会員登録はこちら</a></p>
+    <p><a href="{{ route('register') }}">会員登録はこちら</a></p>
 </body>
 </html>
